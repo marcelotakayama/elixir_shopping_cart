32c0cc126971c24da87364f8e4e38852666a87f0
marcelotakayama <marcelo.takayama00@gmail.com>
2023-09-20 06:38:26 -0300

    Create seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,20 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     ElixirShoppingCart.Repo.insert!(%ElixirShoppingCart.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias ElixirShoppingCart.Repo
+alias ElixirShoppingCart.Cart.Item
+
+%Item{
+  name: "Pencil",
+  description: "Pencil",
+  term_price: 10
+} |> Repo.insert!
+
+%Item{
+  name: "Pen",
+  description: "Pen",
+  term_price: 6
+} |> Repo.insert!
+
+%Item{
+  name: "Eraser",
+  description: "Eraser",
+  term_price: 2
+} |> Repo.insert!
